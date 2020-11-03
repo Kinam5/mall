ee98591581b71d11f6cb4bc5907ce7d24f6dcdc0
wuqinan <804332469@qq.com>
2020-11-03 15:38:18 +0800

    首页开发-网络请求的封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,12 +6,26 @@
   </div>
 </template>
 <script>
-  import NavBar from "components/common/navbar/NavBar";
+  import NavBar from "components/common/navbar/NavBar"
+  import {getHomeMultidata} from "network/home"
 
   export default {
     name: "Home",
     components: {
       NavBar
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      // 1.请求多个数据
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
     }
   };
 </script>
