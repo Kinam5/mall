bd7061a9ee780ab1c446885864ef8919d113916a
wuqinan <804332469@qq.com>
2020-11-03 17:07:47 +0800

    首页开发-featureView组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,12 +5,14 @@
     </nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
+    <feature-view/>
   </div>
 </template>
 <script>
   import NavBar from "components/common/navbar/NavBar"
   import HomeSwiper from "./childComps/HomeSwiper"
-  import RecommendView from "./childComps/RecommendView";
+  import RecommendView from "./childComps/RecommendView"
+  import FeatureView from "./childComps/FeatureView"
 
   import {getHomeMultidata} from "network/home"
 
@@ -19,7 +21,8 @@
     components: {
       NavBar,
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView
     },
     data() {
       return {
@@ -37,8 +40,17 @@
   };
 </script>
 <style scoped>
+  #home {
+    padding-top: 44px;
+    padding-bottom: 49px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #ffffff;
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    z-index: 9;
   }
 </style>
